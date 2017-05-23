6d7212d1d85ea1ade57f5f21d2a7c4109f822118
Stephen Dolenc <stepdo@microsoft.com>
2017-05-23 16:34:27 -0700

    elevate

--- oneTimeSticky/installers.cmd
+++ oneTimeSticky/installers.cmd
@@ -2,9 +2,19 @@
 :: Move working directory to script path
 pushd %~dp0
 
+:: Elevate
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' (
+    echo "Already Admin"
+) else (
+    powershell "saps -filepath %0 -verb runas" >nul 2>&1
+    exit /b
+)
+
 :: Choco
 powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 :: Restore previous path bgefore script
 popd
 
+pause
