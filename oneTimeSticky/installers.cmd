c10d4affad1355e9befba27d9a393b6e7979c839
Stephen Dolenc <stepdo@microsoft.com>
2017-05-23 16:52:55 -0700

    install programs

--- oneTimeSticky/installers.cmd
+++ oneTimeSticky/installers.cmd
@@ -13,6 +13,21 @@ if '%errorlevel%' == '0' (
 :: Choco
 powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
+:: Programs
+choco install -y git
+choco install -y visualstudiocode
+choco install -y notepadplusplus
+choco install -y firefox
+choco install -y googlechrome
+
+:: Verify Repo
+::todo:
+
+:: Configurations
+::todo: ie hyperlink
+choco install -y sourcecodepro
+::todo: n++ configuration
+
 :: Restore previous path bgefore script
 popd
 
